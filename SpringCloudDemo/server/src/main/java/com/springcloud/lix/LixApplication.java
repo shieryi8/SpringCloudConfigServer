8ec1cdc30aa3ef040869e1eaf0cbbf0ce1259cee
LX <398860254@qq.com>
2018-08-28 14:25:08 +0800

    项目初始化

---
+++ SpringCloudDemo/server/src/main/java/com/springcloud/lix/LixApplication.java
@@ -0,0 +1,15 @@
+package com.springcloud.lix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class} /* 忽略数据源加载*/)
+public class LixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LixApplication.class, args);
+    }
+}
