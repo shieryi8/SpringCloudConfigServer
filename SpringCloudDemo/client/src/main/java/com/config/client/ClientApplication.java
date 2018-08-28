3c3312693a6035b4afff366bef51a6187a4e73ac
LX <398860254@qq.com>
2018-08-28 14:26:06 +0800

    项目初始化

---
+++ SpringCloudDemo/client/src/main/java/com/config/client/ClientApplication.java
@@ -0,0 +1,34 @@
+package com.config.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+
+    @Value("${name}")
+    private String name;
+
+    @Value("${age}")
+    private String age;
+
+    @Value("${sex}")
+    private String sex;
+
+    @RequestMapping("hello")
+    public String test1() {
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(sex);
+        return "name=" + name + " sex=" + sex + " age=" + age;
+    }
+
+}
