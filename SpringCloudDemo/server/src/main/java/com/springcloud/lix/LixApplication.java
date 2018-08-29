09549898b30ab2182a020da945d237af1a11f23e
LX <398860254@qq.com>
2018-08-29 15:20:02 +0800

    在原有配置中心中集成Eureka服务注册中心

--- SpringCloudDemo/server/src/main/java/com/springcloud/lix/LixApplication.java
+++ SpringCloudDemo/server/src/main/java/com/springcloud/lix/LixApplication.java
@@ -3,9 +3,11 @@ package com.springcloud.lix;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class} /* 忽略数据源加载*/)
 public class LixApplication {
 
